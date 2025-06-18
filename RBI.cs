34ef964b856d18b2ac682c3c5a337e351912ba4b
kaneriya drashti <143435188+Drashtikaneriya@users.noreply.github.com>
2025-06-18 21:26:10 +0530

    Add files via upload

---
+++ RBI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    internal class RBI
+    {
+        public virtual void calculateInterest()
+        {
+            Console.WriteLine("RBI");
+        }
+    }
+     class HDFC : RBI
+    {
+        public override void calculateInterest()
+        {
+            Console.WriteLine("HDFC");
+        }
+    }
+
+    class SBI : RBI
+    {
+        public override void calculateInterest()
+        {
+            Console.WriteLine("SBI");
+        }
+    }
+    class ICIC : RBI
+    {
+        public override void calculateInterest()
+        {
+            Console.WriteLine("ICIC");
+        }
+    }
+}
